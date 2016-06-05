02b6f08c151c710d931f1a71d2b0af03ce5d645f
Le Doeuff Olivier <olivier.ldff@gmail.com>
2016-06-05 19:54:07 +0200

    Create a rock material

---
+++ Source/ChaosOverride/Weapon/COCataclymer.h
@@ -0,0 +1,19 @@
+// All Right Reserved Olivier Le Doeuff 2015
+
+#pragma once
+
+#include "Weapon/COWeapon.h"
+#include "COCataclymer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHAOSOVERRIDE_API ACOCataclymer : public ACOWeapon
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/ChaosOverride/Weapon/COInterrogator.h
@@ -0,0 +1,19 @@
+// All Right Reserved Olivier Le Doeuff 2015
+
+#pragma once
+
+#include "Weapon/COWeapon.h"
+#include "COInterrogator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHAOSOVERRIDE_API ACOInterrogator : public ACOWeapon
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
